3570467476731bb235a56b3208d6db7b1bc5fa54
SemEtic17 <semetic17@gmail.com>
2024-03-23 10:57:11 +0300

    Hidding api key

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDibpsjjWW1Yh3ZgN5baUssqPmDyuMTRJ8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "hsl-videos.firebaseapp.com",
   projectId: "hsl-videos",
   storageBucket: "hsl-videos.appspot.com",
@@ -15,13 +15,6 @@ const firebaseConfig = {
   measurementId: "G-27DS9W3K6Y",
 };
 
-// apiKey: "AIzaSyDibpsjjWW1Yh3ZgN5baUssqPmDyuMTRJ8",
-//   authDomain: "hsl-videos.firebaseapp.com",
-//   projectId: "hsl-videos",
-//   storageBucket: "hsl-videos.appspot.com",
-//   messagingSenderId: "1094207130034",
-//   appId: "1:1094207130034:web:48667388a022edd804bcab",
-//   measurementId: "G-27DS9W3K6Y",
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);

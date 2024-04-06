344cb90a09e46f420131be871442115acb0651cb
SemEtic17 <semetic17@gmail.com>
2024-04-06 12:29:12 +0300

    Adding the profile and signin in smaller screens

--- client/src/components/CardList.jsx
+++ client/src/components/CardList.jsx
@@ -3,17 +3,75 @@ import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import {Button} from 'flowbite-react';
+import { Avatar, Dropdown } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { signoutSuccess } from '../redux/user/userSlice';
 
 //* Displaying the HSL Activities by looping the Card component
 
 export default function CardList({ Robots }) {
   const dispatch = useDispatch();
   const { theme } = useSelector((state) => state.theme);
+  const { currentUser } = useSelector((state) => state.user);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="container ml">
       <div id="activities">
-      <div className="parent-container">  
-      <h4 className="mb-5 d-flex align-items-center justify-content-center text-center lg:ml-[33rem] md:ml-[33rem] ml-[3rem]">
+      <div className="parent-container">
+      {currentUser ? (
+        <div className='mb-[35px] lg:hidden'>
+                  <Dropdown
+                    arrowIcon={false}
+                    inline
+                    label={
+                      <Avatar alt='user' img={currentUser.profilePicture} rounded />
+                    }
+                  >
+                    <Dropdown.Header>
+                      <span className='block text-sm'>@{currentUser.username}</span>
+                      <span className='block text-sm font-medium truncate'>
+                        {currentUser.email}
+                      </span>
+                    </Dropdown.Header>
+                    <Dropdown.Item>
+                      <Link to='/dashbord'>Ckeck assighnments</Link>
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <a href={'/dashboard?tab=profile'}>
+                      <Dropdown.Item>Profile</Dropdown.Item>
+                    </a>
+                    <Dropdown.Divider />
+                    <Dropdown.Item onClick={handleSignout}><Link to='/'>
+                      Sign Out
+                    </Link></Dropdown.Item>
+                  </Dropdown>
+              </div>
+                ) : (
+                  
+                  <a href='#sign' className='w-[6rem] h-[2rem] mb-[35px] lg:hidden'>
+                    <Button className='' gradientDuoTone='purpleToBlue' outline>
+                      Sign Up
+                    </Button>
+                  </a>
+              
+                )}
+      <h4 className="mb-5 d-flex align-items-center justify-content-center text-center lg:ml-[33rem] md:ml-[33rem] ml-[-1rem]">
   <strong>Home Sweet Land Activities</strong>
 </h4>
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -50,15 +50,14 @@ export default function Header() {
 
   return (
     <Navbar className='border-b-2'>
-      <Link
-        to='/'
+      <div
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
           Homesweetland
         </span>
         <span className="text-white text-xl">---</span>Assignments
-      </Link>
+      </div>
       <form onSubmit={handleSubmit}>
         <TextInput
           type='text'
@@ -95,6 +94,10 @@ export default function Header() {
                 {currentUser.email}
               </span>
             </Dropdown.Header>
+            <Dropdown.Item>
+                <Link to='/'>Homepage</Link>
+              </Dropdown.Item>
+            <Dropdown.Divider />
             <Link to={'/dashboard?tab=profile'}>
               <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
